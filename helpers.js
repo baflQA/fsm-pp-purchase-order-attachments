c007f680101a187748b49d50e9cf57d703610d5e
Maciej Pieńkowski <m.pienkowski@sap.com>
2021-11-09 11:23:51 +0100

    fix empty token

--- helpers.js
+++ helpers.js
@@ -1,18 +1,21 @@
 let token;
 
 function initializeRefreshTokenStrategy(shellSdk, auth) {
-    shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, (event) => {
-        token = event.access_token;
-        setTimeout(() => fetchToken(), (event.expires_in * 1000) - 5000);
-    });
-
-    function fetchToken() {
-        shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
-            response_type: 'token'  // request a user token within the context
+    return new Promise((resolve) => {
+        shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, (event) => {
+            token = event.access_token;
+            resolve(token);
+            setTimeout(() => fetchToken(), (event.expires_in * 1000) - 5000);
         });
-    }
 
-    setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 5000);
+        function fetchToken() {
+            shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
+                response_type: 'token'  // request a user token within the context
+            });
+        }
+
+        setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 5000);
+    });
 }
 
 
